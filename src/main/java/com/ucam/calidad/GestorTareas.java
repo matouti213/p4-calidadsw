c0ad5824c7f37071886b1ead5923247335a81d46
Matouti <matouti213@gmail.com>
2025-12-16 16:57:17 +0100

    Error linea solucionado

--- src/main/java/com/ucam/calidad/GestorTareas.java
+++ src/main/java/com/ucam/calidad/GestorTareas.java
@@ -33,6 +33,5 @@ public class GestorTareas {
         return contador;
     }
     
-    private String lineaMuyLarga = "aaaaaaaaaaaadadadadaaaaaaaqqqaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 }
