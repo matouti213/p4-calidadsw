4100b94b131e9957a904006788e8e4fc2a564db3
Matouti <matouti213@gmail.com>
2025-12-16 16:57:59 +0100

    Error linea2

--- src/main/java/com/ucam/calidad/GestorTareas.java
+++ src/main/java/com/ucam/calidad/GestorTareas.java
@@ -33,5 +33,6 @@ public class GestorTareas {
         return contador;
     }
     
+    private String lineaMuyLarga = "aaaaaaaaaaaadadadadaaaaaaaqqqaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 }
